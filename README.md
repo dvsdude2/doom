9e23d7f243e1ff94e0ac1d1f0cabcb8aa977eb69
dvsdude2 <dvsdude@protonmail.com>
2021-12-05 21:22:16 -0700

    this is my new base line. quick readme upgrade

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
 # doom
 my doom-Emacs configs
+
+
+**<h2 align="left">MY DEADLY DOOM CONFIG</h2>**                  
 ![DOOM](/splash/doom-emacs-slant-out-color.png)
+
+**<h3 align="left">description</h3>**
+_______________________________
+#### More than just dotconfigs
+
+    keep configs to save the work I put into making them.
+    plan on keeping a lot of my arch set up here. so not all config files
+    the plan is to use these settings for any new builds in future.
+
+
+#### Installation
+________________________________
+
+**STEP_ONE**
+if already installed.
+  * [ ] backup your .emacs.d folder. to do this,
+simply put .bak at the end of the file name.
+
+**STEP_TWO**
+  * [ ] download and install a brand spaking new version of Emacs.
+after install.
+
+**STEP_THREE**
+  * [ ] download and install a new version of Doom
+follow the instructions on this page: https://github.com/hlissner/doom-emacs
+once your new version of Doom-Emacs is up and running.
+
+**STEP_FOUR**
+  * [ ] clone this repository. check if packages need downloading first.
+ensure t does not work with new package manager straight.el 
+
+use [**git**  ](https://git-scm.com/ "download git") to clone
+
+```python
+git clone https://github.com/dvsdude2/doom
+```
+
+**STEP_FIVE**
+  * [ ] use the repo files to get back to this set up.
+
+#### Usage
+________________________________
+
+
+    after clean install of a new ISO
+    use these files to get back all the customizing
+
+#### Contributing
+________________________________
+Pull requests are welcome. For major changes, please open an issue first to
+discuss what you would like to change.
+
+If you want to make major changes.....just clone the repo and change them there.
+
+#### License
+_______________________________
+[MIT ](https://choosealicense.com/licenses/mit/)
--- config.el
+++ config.el
@@ -266,81 +266,87 @@
    ;; (add-hook 'mail-mode-hook 'ac-ispell-ac-setup)
 
 
-(require 'orderless)
-(setq completion-styles '(orderless))
+;; (require 'orderless)
+;; (setq completion-styles '(orderless))
 
 ;;;; company ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; =============================================================================
 
-(use-package company
-  :config
-  (setq company-idle-delay 0
-        company-minimum-prefix-length 3
-        company-selection-wrap-around t))
-(global-company-mode)
-#'+end_src
+;; (use-package company
+;;   :config
+;;   (setq company-idle-delay 0
+;;         company-minimum-prefix-length 3
+;;         company-selection-wrap-around t))
+;; (global-company-mode)
 
 ;;;; marginalia ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable richer annotations using the Marginalia package ;;;;;;;;;;;;;;;;;;;;;;
 ;; =============================================================================
 
 (use-package marginalia
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
-  :bind (("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-
   ;; The :init configuration is always executed (Not lazy!)
   :init
-
   ;; Must be in the :init section of use-package such that the mode gets
   ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode))
 
-;;;; vertico ;;;;;;;;;;;;;;;;;;;;;;;;;;as reveiwed by ;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;https://systemcrafters.cc/emacs-tips/streamline-completions-with-vertico/
-;; =============================================================================
+;;;; VERTICO ;;;;;;;;;;;;home page;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;==============================================================================
 
 (use-package vertico
   :init
-  (vertico-mode))
+  (vertico-mode)
+  (setq vertico-cycle t))
 (use-package orderless
   :init
-;; (setq completion-styles '(basic substring partial-completion flex)
-;; (setq completion-styles '(substring orderless)
+  ;; (setq completion-styles '(basic substring partial-completion flex))
+  ;; (setq completion-styles '(substring orderless))
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
-;; this is to add the vim keybindings
-(use-package vertico
-  :bind (:map vertico-map
-         ("C-j" . vertico-next)
-         ("C-k" . vertico-previous)
-         ("C-f" . vertico-exit)
-         :map minibuffer-local-map
-         ("M-h" . backward-kill-word))
-  :custom
-  (vertico-cycle t)
-  :init
-  (vertico-mode))
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
 (use-package savehist
   :init
   (savehist-mode))
+(use-package emacs
+  :init
+  ;; Alternatively try `consult-completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+;; Use `consult-completion-in-region' if Vertico is enabled.
+;; Otherwise use the default `completion--in-region' function.
+(setq completion-in-region-function
+      (lambda (&rest args)
+        (apply (if vertico-mode
+                   #'consult-completion-in-region
+                 #'completion--in-region)
+               args)))
+(advice-add #'completing-read-multiple
+            :override #'consult-completing-read-multiple)
+(setq org-refile-use-outline-path 'file
+      org-outline-path-complete-in-steps nil)
+(advice-add #'tmm-add-prompt :after #'minibuffer-hide-completions)
 (use-package marginalia
   :after vertico
   :custom
   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
   :init
   (marginalia-mode))
-
 ;;;; corfu ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; =============================================================================
 
 (use-package corfu
   ;; Optional customizations
-  :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
   ;; (corfu-auto t)                 ;; Enable auto completion
   ;; (corfu-commit-predicate nil)   ;; Do not commit selected candidates on next input
   ;; (corfu-quit-at-boundary t)     ;; Automatically quit at word boundary
@@ -360,36 +366,22 @@
   :init
   (corfu-global-mode))
 
-;; Optionally use the `orderless' completion style. See `+orderless-dispatch'
-;; in the Consult wiki for an advanced Orderless style dispatcher.
-;; Enable `partial-completion' for files to allow path expansion.
-;; You may prefer to use `initials' instead of `partial-completion'.
 (use-package orderless
   :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  ;; (setq completion-styles '(basic substring partial-completion flex))
+  ;; (setq completion-styles '(substring orderless))
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles . (partial-completion))))))
-
 ;; Use dabbrev with Corfu!
 (use-package dabbrev
   ;; Swap M-/ and C-M-/
   :bind (("M-/" . dabbrev-completion)
          ("C-M-/" . dabbrev-expand)))
-
-;; A few more useful configurations...
 (use-package emacs
   :init
   ;; TAB cycle if there are only few candidates
   (setq completion-cycle-threshold 3)
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
   (setq tab-always-indent 'complete))
@@ -501,8 +493,6 @@
   :hook (completion-list-mode . consult-preview-at-point-mode)
 )
 
-;;; Enable recursive minibuffers ;;;;;;;
-(setq enable-recursive-minibuffers t)
 ;;; ignore-case ;;;;;;;;;;;;;;;;;;;;;;;;
 (setq read-file-name-completion-ignore-case t
       read-buffer-completion-ignore-case t
@@ -554,7 +544,6 @@
 
 (setq bookmark-save-flag 1)
 ;; (defcustom bookmark-save-flag 1)
-;#+end_src
 
 ;;;; personal keybindings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; =============================================================================
