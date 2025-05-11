7fbddc8901ff2fe14dcf2715fd45784664be61a8
dvsdude2 <dvsdude@protonmail.com>
2025-05-11 02:11:36 -0600

    switching to dirvish need to tweak dired
    - subtree should not be needed, not sure about the rest?

--- config.el
+++ config.el
@@ -289,12 +289,6 @@
   :config
   (ready-player-mode +1))
 
-(use-package! dired-subtree
-  :after dired
-  :config
-  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
-  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
-
 (after! dired
   (use-package! dired-open
     :config
