86617911ec715ec7af98c06c3f05d462d8a1ba9e
dvsdude2 <dvsdude@protonmail.com>
2025-08-03 20:25:44 -0600

    [elfeed-summary] scrap tangling file, reformat.
    - Using a literate programming with this was a not useful at all. It did
    not work for editing, there was no good way to keep the formatting. So I
    will chalk it up to a learning experience. Now I know first hand how to
    do it, I just should not have picked a variable to split up. Well now I
    know, it sounded good on paper?

--- myrepo/elfeed-summary-layout/+elfeed-summary-settings.el
+++ myrepo/elfeed-summary-layout/+elfeed-summary-settings.el
@@ -16,124 +16,95 @@
                (:hide t))
         (group (:title . "searches Days")
                (:elements
-                (group
-                 (:title . "2 days")
-                 (:elements
-                  (search
-                   (:filter . "@2-day-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "3 days")
-                 (:elements
-                  (search
-                   (:filter . "@3-day-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "4 days")
-                 (:elements
-                  (search
-                   (:filter . "@4-day-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "1 week")
-                 (:elements
-                  (search
-                   (:filter . "@7-day-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "2 weeks")
-                 (:elements
-                  (search
-                   (:filter . "@2-weeks-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "3 weeks")
-                 (:elements
-                  (search
-                   (:filter . "@3-weeks-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "1 month")
-                 (:elements
-                  (search
-                   (:filter . "@1-month-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "2 months")
-                 (:elements
-                  (search
-                   (:filter . "@2-month-ago")
-                   (:title . "")))
-                 (:hide t))
-                (group
-                 (:title . "6 months")
-                 (:elements
-                  (search
-                   (:filter . "@6-months-ago +unread")
-                   (:title . "+unread"))
-                  (search
-                   (:filter . "@6-months-ago")
-                   (:title . "+all")))))
+                (group (:title . "2 days")
+                       (:elements
+                        (search (:filter . "@2-day-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "3 days")
+                       (:elements
+                        (search (:filter . "@3-day-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "4 days")
+                       (:elements
+                        (search (:filter . "@4-day-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "1 week")
+                       (:elements
+                        (search (:filter . "@7-day-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "2 weeks")
+                       (:elements
+                        (search (:filter . "@2-weeks-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "3 weeks")
+                       (:elements
+                        (search (:filter . "@3-weeks-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "1 month")
+                       (:elements
+                        (search (:filter . "@1-month-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "2 months")
+                       (:elements
+                        (search (:filter . "@2-month-ago")
+                                (:title . "")))
+                       (:hide t))
+                (group (:title . "6 months")
+                       (:elements
+                        (search (:filter . "@6-months-ago +unread")
+                                (:title . "+unread"))
+                        (search (:filter . "@6-months-ago")
+                                (:title . "+all")))))
                (:hide t))
         (group (:title . "stared")
                (:elements
-                (search
-                 (:filter . "+star")
-                 (:title . "")))
+                (search (:filter . "+star")
+                        (:title . "")))
                (:hide t))
         (group (:title . "Videos")
                (:elements
-                (group
-                 (:title . "truth")
-                 (:elements
-                  (query . (and vid truth)))
-                 (:hide t))
-                (group
-                 (:title . "humor")
-                 (:elements
-                  (query . (and vid fun)))
-                 (:hide t))
-                (group
-                 (:title . "real")
-                 (:elements
-                  (query . (and vid real)))
-                 (:hide t))
-                (group
-                 (:title . "history")
-                 (:elements
-                  (query . (and vid hist)))
-                 (:hide t))
-                (group
-                 (:title . "emacs")
-                 (:elements
-                  (query . (and vid emacs)))
-                 (:hide t))
-                (group
-                 (:title . "websites")
-                 (:elements
-                  (query . (and vid web)))
-                 (:hide t))
-                (group
-                 (:title . "youtube")
-                 (:elements
-                  (query . (and vid yt)))
-                 (:hide t))
-                (group
-                 (:title . "odysee")
-                 (:elements
-                  (query . (and vid odys)))
-                 (:hide t))
-                (group
-                 (:title . "bitchute")
-                 (:elements
-                  (query . (and vid bit)))))
+                (group (:title . "truth")
+                       (:elements
+                        (query . (and vid truth)))
+                       (:hide t))
+                (group (:title . "humor")
+                       (:elements
+                        (query . (and vid fun)))
+                       (:hide t))
+                (group (:title . "real")
+                       (:elements
+                        (query . (and vid real)))
+                       (:hide t))
+                (group (:title . "history")
+                       (:elements
+                        (query . (and vid hist)))
+                       (:hide t))
+                (group (:title . "emacs")
+                       (:elements
+                        (query . (and vid emacs)))
+                       (:hide t))
+                (group (:title . "websites")
+                       (:elements
+                        (query . (and vid web)))
+                       (:hide t))
+                (group (:title . "youtube")
+                       (:elements
+                        (query . (and vid yt)))
+                       (:hide t))
+                (group (:title . "odysee")
+                       (:elements
+                        (query . (and vid odys)))
+                       (:hide t))
+                (group (:title . "bitchute")
+                       (:elements
+                        (query . (and vid bit)))))
                (:hide t))
         (group (:title . "forums")
                (:elements
@@ -173,8 +144,6 @@
                (:hide t))
         (group (:title . "searches all")
                (:elements
-                (group
-                 (:title . "ungrouped")
-                 (:elements :misc))))))
-
+                (group (:title . "ungrouped")
+                       (:elements :misc))))))
 ;;; +elfeed-summary-settings.el ends here
