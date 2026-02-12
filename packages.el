1a67e821ea318ea73bcc9df974108ab69d51c714
dvsdude2 <dvsdude@protonmail.com>
2026-02-11 20:51:37 -0700

    [dimmer.el] added package that dims unfocused.
    - Not sure if I will keep this, but will give it a try.

--- packages.el
+++ packages.el
@@ -55,6 +55,7 @@
 (package! yasnippet-capf :pin "f53c42a996b86fc95b96bdc2deeb58581f48c666")
 (package! dashboard :pin "e9867036ebc100768023eabc21dd5bf4619029c2")
 (package! define-word)
+(package! dimmer)
 (package! dired-preview)
 (package! dired-open)
 (package! dwim-shell-command)
