7bf87ca119a199a8a08f5493692316837b90b975
dvsdude2 <dvsdude@protonmail.com>
2026-06-11 16:17:08 -0600

    [unison] will need to be updated to new location.
    - This is confusing me. When I first added the package. This was the way
    I read to sync two files. Something has changed in the way it is to
    used. ??
    - I also have a gui version of this package but it seems different from
    the one I started with.
    - Too add to the confusion is the fact that Emacs and Doom will not stop
    moving every fucking thing from one place to another.
    - I could rant about this for a half hour. A little stability won't
    hurt.
    - The last few years felt like everything down to its core needed to be
    changed moved and rewritten. Fuck! enough already! But NO! We need big
    radical change or else!
    - Will wait and give up on this idea of keeping a automated backup for
    my personal modules till there is some stability.

--- modules/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-
-((nil . ((unison-sync-root1 . "/home/dvsdude/.config/doom/modules/corfudvs")
-         (unison-sync-root2 . "/home/dvsdude/.config/emacs/modules/completion/corfudvs")
-         (unison-sync-excluded . ("*.tmp" "*.log"))
-         (unison-sync-one-way-sync . nil))))
