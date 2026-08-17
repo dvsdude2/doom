4c5f41c7d5388ffa9e8bb85f06aaa5e4bd81227d
dvsdude2 <dvsdude@protonmail.com>
2026-08-16 19:44:05 -0600

    doom -> 2.2.2 which key is part of core not needed.
    - What I can understand is this is now part of core and this is not
    needed? maybe?

--- config.el
+++ config.el
@@ -1230,28 +1230,6 @@ link and copy to kill ring."
 (key-chord-define evil-normal-state-map "vv" 'evil-visual-line)
 (key-chord-define evil-normal-state-map "cx" 'evilnc-comment-or-uncomment-lines)
 
-(use-package! which-key
-  :hook (doom-first-input . which-key-mode)
-  :init
-  (setq which-key-sort-order #'which-key-key-order-alpha
-        which-key-sort-uppercase-first nil
-        which-key-add-column-padding 1
-        which-key-idle-delay 1.5
-        which-key-max-display-columns nil
-        which-key-min-display-lines 6
-        which-key-side-window-slot -10)
-  :config
-  (put 'which-key-replacement-alist 'initial-value which-key-replacement-alist)
-  (add-hook! 'doom-before-reload-hook
-    (defun doom-reset-which-key-replacements-h ()
-      (setq which-key-replacement-alist (get 'which-key-replacement-alist 'initial-value))))
-  ;; general improvements to which-key readability
-  (which-key-setup-side-window-bottom)
-  (setq-hook! 'which-key-init-buffer-hook line-spacing 3)
-
-  (which-key-add-key-based-replacements doom-leader-key "<leader>")
-  (which-key-add-key-based-replacements doom-localleader-key "<localleader>"))
-
 ;; mpv commands
 
 ;; make mpv type link
